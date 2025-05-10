040b8b4dc147c472fb442d0cae375595c9455a13
thedabbler24 <69151207+thedabbler24@users.noreply.github.com>
2025-05-10 12:43:21 +0300

    Created using Colab

--- simple_example.py
+++ simple_example.py
@@ -4,8 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyNMncO+lyfKl6uKcn2Fb+KR",
-      "include_colab_link": true
+      "authorship_tag": "ABX9TyNMncO+lyfKl6uKcn2Fb+KR"
     },
     "kernelspec": {
       "name": "python3",
@@ -16,16 +15,6 @@
     }
   },
   "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/thedabbler24/gw-mem-analysis/blob/main/simple_example.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
     {
       "cell_type": "code",
       "execution_count": null,
